439e128626ce4a5a8efee1c6bb069ba2d9c5c2a5
Designrpros <Designrpros@.github.com>
2024-03-03 17:00:18 +0100

    wiggle room

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,6 @@ import HeroSection from '../component/HeroSection';
 import AboutSection from '../component/AboutSection';
 import CoffeeCategories from '../component/CoffeeCategories';
 import ProductShowcase from '../component/ProductShowcase';
-
 import GallerySection from '../component/GallerySection';
 import CooperationSection from '../component/CooperationSection';
 import ContactSection from '../component/ContactSection'; // Adjust the import path as necessary
