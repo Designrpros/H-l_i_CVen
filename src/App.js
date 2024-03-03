1549d3a8b070a8e924f880e294be850b13c50caf
Designrpros <Designrpros@.github.com>
2024-03-03 16:36:45 +0100

    global font for headers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Toolbar from './component/Toolbar';
 import Home from './Pages/Home'; // Import the Home component
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import './App.css';
 
 const StyledWrapper = styled.div`
   text-align: center;
