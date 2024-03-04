8866d821e8a19467052256d0f6eb607aff10c097
Designrpros <Designrpros@.github.com>
2024-03-04 20:16:49 +0100

    initial ecommerce

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HeroSection from '../component/HeroSection';
 import AboutSection from '../component/AboutSection';
 import CoffeeCategories from '../component/CoffeeCategories';
