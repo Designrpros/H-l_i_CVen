752ecd791bb12df0430d1facd1c1bf8547116a53
Designrpros <Designrpros@.github.com>
2024-03-05 05:05:10 +0100

    Cors headers

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,16 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust accordingly for production
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle OPTIONS method for preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).end('Method Not Allowed');
     }
