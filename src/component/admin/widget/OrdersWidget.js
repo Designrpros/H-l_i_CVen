32ef0bc025638e006dba1edf53c9d9ea2760f14d
Designrpros <Designrpros@.github.com>
2024-03-07 04:28:27 +0100

    admin widgets

--- src/component/admin/widget/OrdersWidget.js
+++ src/component/admin/widget/OrdersWidget.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../../../firebaseConfig'; // Adjust import path as necessary
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 
 const OrdersWidget = () => {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "orders"), (snapshot) => {
-      const newOrders = snapshot.docs.map(doc => doc.data());
+    // Adjust the query to include orderBy
+    const q = query(collection(db, "orders"), orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const newOrders = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+        // Convert timestamp to JavaScript Date object if necessary
+        createdAt: doc.data().createdAt?.toDate().toString(),
+      }));
+      console.log(newOrders); // Log fetched data
       setOrders(newOrders);
     });
 
@@ -19,7 +27,27 @@ const OrdersWidget = () => {
   return (
     <div>
       <h4>Total Revenue: ${totalRevenue.toFixed(2)}</h4>
-      {/* Further UI elements to display orders data */}
+      <ul>
+        {orders.map((order) => (
+          <li key={order.id}>
+            <p>Order ID: {order.id}</p>
+            <p>Email: {order.email}</p>
+            <p>Total Amount: ${order.totalAmount / 100}</p> {/* Assuming totalAmount is in cents */}
+            <p>Order Date: {order.createdAt}</p>
+            <p>Products:</p>
+            <ul>
+              {order.productsPurchased.map((product, index) => (
+                <li key={index}>
+                  {product.name} - Quantity: {product.quantity} - Unit Price: ${product.unitPrice / 100}
+                </li>
+              ))}
+            </ul>
+            {order.shippingDetails && (
+              <p>Shipping Address: {Object.values(order.shippingDetails.address).join(', ')}</p>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
