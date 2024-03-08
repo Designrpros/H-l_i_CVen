50f17b54a1cf4c2024c8d02ea5c69bdd22beb0a2
Designrpros <Designrpros@.github.com>
2024-03-08 11:43:24 +0100

    analytics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import Home from './Pages/Home';
 import ProductShowcase from './component/ProductShowcase';
-import ProductListing from './component/ProductListing'; 
+import ProductListing from './component/ProductListing';
 import Cart from './component/Cart';
-import Dashboard from './component/admin/Dashboard'; // Import the Dashboard component
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import './App.css';
+import Dashboard from './component/admin/Dashboard';
 import Toolbar from './component/Toolbar';
 import SuccessPage from './component/SuccessPage';
 import CancelPage from './component/CancelPage';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './App.css';
+
+// Initialize GA4 with your Measurement ID
+ReactGA.initialize('G-ND7ZZY1GLW');
+
+function usePageViews() {
+  let location = useLocation();
+  useEffect(() => {
+    // Track page view on route change
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+}
 
 function App() {
+  usePageViews(); // Call our page view hook
+
   return (
     <Router>
       <Toolbar />
@@ -23,7 +37,6 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/success" element={<SuccessPage />} />
         <Route path="/cancel" element={<CancelPage />} />
-        {/* Correctly placed Dashboard route */}
         <Route path="/admin/*" element={<Dashboard />} />
       </Routes>
     </Router>
