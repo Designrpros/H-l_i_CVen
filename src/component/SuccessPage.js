c6d7d8c60c254c08d5388806804fc1b493a7154e
Designrpros <Designrpros@.github.com>
2024-03-08 03:30:25 +0100

    admin nice

--- src/component/SuccessPage.js
+++ src/component/SuccessPage.js
@@ -6,8 +6,6 @@ const Container = styled.div`
   max-width: 600px;
   margin: 40px auto;
   padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   text-align: center;
 `;
 
